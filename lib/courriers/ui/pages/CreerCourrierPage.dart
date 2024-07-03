b7dd3eac0a2535f202ac88e2463c2cb5213d93b8
Kunda <izakikunda@gmail.com>
2024-07-03 15:00:08 +0100

    update_creercourrierpage.dart

--- lib/courriers/ui/pages/CreerCourrierPage.dart
+++ lib/courriers/ui/pages/CreerCourrierPage.dart
@@ -8,16 +8,125 @@ class CreerCourrierPage extends StatefulWidget {
 }
 
 class _CreerCourrierPageState extends State<CreerCourrierPage> {
+
+  var objetCrtl = TextEditingController(text: "");
+  var destCrtl = TextEditingController(text: "");
+  String expediteur = 'Exp 1';
+  // Liste des expediteur
+  var expediteurs = [
+    'Exp 1',
+    'Exp 2',
+    'Exp 3',
+    'Exp 4',
+    'Exp 5',
+  ];
+
+  String agent = 'Agents 1';
+  // Liste des agents
+  var agents = [
+    'Agents 1',
+    'Agents 2',
+    'Agents 3',
+    'Agents 4',
+    'Agents 5',
+  ];
+
+  String type = 'Type 1';
+  // Liste des types de courriers
+  var types = [
+    'Type 1',
+    'Type 2',
+    'Type 3',
+    'Type 4',
+    'Type 5',
+  ];
+
+  String entite = 'Entite 1';
+  // Liste des entités
+  var entites = [
+    'Entite 1',
+    'Entite 2',
+    'Entite 3',
+    'Entite 4',
+    'Entite 5',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         children: [
           //Objet
-          TextFormField(),
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Objet',
+            ),
+            controller: objetCrtl,
+          ),
+          const SizedBox(height: 5,),
           //Expediteur
-          TextFormField(),
-          //
+          DropdownButton(
+            value: expediteur,
+              items: expediteurs.map((String items) {
+                return DropdownMenuItem(
+                  value: items,
+                    child: Text(items),
+                );
+              }).toList(),
+              onChanged: (String? newValue){
+                setState(() {
+                  expediteur = newValue!;
+                });
+              }
+          ),
+          const SizedBox(height: 5,),
+          //Destinataire
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Destinataire',
+            ),
+            controller: destCrtl,
+          ),
+          const SizedBox(height: 5,),
+          //Type
+          DropdownButton(
+              value: type,
+              items: types.map((String items) {
+                return DropdownMenuItem(
+                  value: items,
+                  child: Text(items),
+                );
+              }).toList(),
+              onChanged: (String? newValue){
+                setState(() {
+                  type = newValue!;
+                });
+              }
+          ),
+          const SizedBox(height: 5,),
+          //Agents
+          DropdownButton(
+              value: agent,
+              items: agents.map((String items) {
+                return DropdownMenuItem(
+                  value: items,
+                  child: Text(items),
+                );
+              }).toList(),
+              onChanged: (String? newValue){
+                setState(() {
+                  agent = newValue!;
+                });
+              }
+          ),
+          const SizedBox(height: 5,),
+          //dateEmission
+          InputDatePickerFormField(firstDate: DateTime(2020), lastDate: DateTime(2024)),
+          const SizedBox(height: 5,),
+          //dateReception
+          InputDatePickerFormField(firstDate: DateTime(2020), lastDate: DateTime(2024)),
+          const SizedBox(height: 5,),
+          ElevatedButton(onPressed: onPressed, child: Text('Enregistrer')),
         ],
       ),
     );
